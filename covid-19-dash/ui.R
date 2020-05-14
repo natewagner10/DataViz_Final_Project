3dffaa2e111ddcc63ca3fc76ffce15b081756750
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-05-14 09:41:54 -0400

    Update ui.R

--- covid-19-dash/ui.R
+++ covid-19-dash/ui.R
@@ -1,13 +1,21 @@
 library(shiny)
 library(shinydashboard)
 library(readr)
-
 filePath = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 dff10 <- read_csv(filePath) %>% filter(state == "Florida")
 dashboardPage(
     dashboardHeader(title = "FL Covid-19"),
     dashboardSidebar(),
     dashboardBody(
-        tabItems()
+        tabItems(tabItem(
+                tabName = "TAB1", 
+                fluidRow(
+                        box(width=6, height = 550,
+                        status="info",
+                        title = "Data from New York Times",  
+                        solidHeader = TRUE,
+                        DTOutput("mydata")
+                    )                
+            ))
     )
 )
